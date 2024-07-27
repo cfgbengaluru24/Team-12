1be69fdcaf61f83645c35ec5103ef3ef14d85d17
Krrish <malekm09122003@gmail.com>
2024-07-27 20:03:02 +0530

    Added baseline and endline import routes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('healthcheck', views.healthcheck, name='healthcheck'),
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+    path('state_wise_shg', views.state_wise_shg, name='state_wise_shg'),
+    path('import_baseline', views.import_baseline, name='import_baseline'),
+    path('import_endline', views.import_endline, name='import_endline'),
 ]
